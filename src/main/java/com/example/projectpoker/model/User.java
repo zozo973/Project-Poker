fd6bc6bf4ec1d5f6d0d07708b80c4201ad553bd1
Basilis Karalouka <basi.loukas1@gmail.com>
2026-03-27 14:28:14 +1000

    Adding Fields in User Class

--- src/main/java/com/example/projectpoker/model/User.java
+++ src/main/java/com/example/projectpoker/model/User.java
@@ -1,4 +1,11 @@
 package com.example.projectpoker.model;
 
+import java.time.LocalDateTime;
+
 public class User {
+    private int UserID;
+    private String userName;
+    private String userPassword;
+    private String email;
+    private LocalDateTime createdAt;
 }

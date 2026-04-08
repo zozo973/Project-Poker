f3cefbedc144f4bb649b574cbf4a43dbe49fe29c
Basilis Karalouka <basi.loukas1@gmail.com>
2026-04-08 22:42:21 +1000

    added getter and setter methods to access private fields

--- src/main/java/com/example/projectpoker/model/User.java
+++ src/main/java/com/example/projectpoker/model/User.java
@@ -5,19 +5,53 @@ import java.time.LocalDateTime;
 public class User {
     // fields that go into the User Database table
     private int UserID;
-    private String userName;
-    private String userPassword;
-    private String email;
-    private LocalDateTime createdAt;
+    private String Username;
+    private String Password;
+    private String Email;
+    private LocalDateTime CreatedAt;
 
     // add constructor to set initial values for object fields
     // method takes parameters which will be set by user
-    private User(String userName, String userPassword, String email) {
-        this.userName = userName;
-        this.userPassword = userPassword;
-        this.email = email;
+    // UserID and LocalDateTime aren't provided by user directly, they are created differently
+    public User(String Username, String Password, String Email) {
+        this.Username = Username;
+        this.Password = Password;
+        this.Email = Email;
     }
 
+    // get method returns variable value
+    public int GetUserID() {
+        return UserID;
+    }
+    public String GetUsername() {
+        return Username;
+    }
+    public String GetPassword() {
+        return Password;
+    }
+    public String GetEmail() {
+        return Email;
+    }
+    public LocalDateTime GetCreatedAt() {
+        return CreatedAt;
+    }
+
+    // set method sets variable value
+    public void SetUserID(int UserID){
+        this.UserID = UserID;
+    }
+    public void SetUsername(String Username) {
+        this.Username = Username;
+    }
+    public void SetPassword(String Password){
+        this.Password = Password;
+    }
+    public void SetEmail(String Email){
+        this.Email = Email;
+    }
+    public void SetCreatedAt(LocalDateTime CreatedAt){
+        this.CreatedAt = CreatedAt;
+    }
 }
 
 

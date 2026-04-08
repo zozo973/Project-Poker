b99e59458c97e0577299d353e0c8ae20e0613dd5
Basilis Karalouka <basi.loukas1@gmail.com>
2026-04-08 23:10:21 +1000

    adding fields, constructors, getting and setting methods to SessionManager.java

--- src/main/java/com/example/projectpoker/session/SessionManager.java
+++ src/main/java/com/example/projectpoker/session/SessionManager.java
@@ -1,4 +1,29 @@
 package com.example.projectpoker.session;
 
+import java.time.LocalDateTime;
+
+// app needs to remember who is logged in for as long as the app is open
 public class SessionManager {
+
+    // field (not sure if should be public or private)
+    // set as boolean cause either logged in or logged out
+        // true = logged in
+        // false = logged out
+    private static Boolean CurrentUser;
+
+    // constructor
+    public SessionManager(Boolean CurrentUser){
+        this.CurrentUser = CurrentUser;
+    }
+    public Boolean GetCurrentUser() {
+        return GetCurrentUser();
+    }
+    public void SetCurrentUser_In(Boolean CurrentUser){
+        CurrentUser = true;
+        this.CurrentUser = CurrentUser;
+    }
+    public void SetCurrentUser_Out(Boolean CurrentUser){
+        CurrentUser = false;
+        this.CurrentUser = CurrentUser;
+    }
 }

cf2306872912518359ffca3a026f51307d2707a5
Basilis Karalouka <basi.loukas1@gmail.com>
2026-04-08 23:15:57 +1000

    editting and fixing up bugs in SessionManager.java

--- src/main/java/com/example/projectpoker/session/SessionManager.java
+++ src/main/java/com/example/projectpoker/session/SessionManager.java
@@ -1,29 +1,23 @@
 package com.example.projectpoker.session;
 
-import java.time.LocalDateTime;
-
 // app needs to remember who is logged in for as long as the app is open
 public class SessionManager {
 
-    // field (not sure if should be public or private)
+    // field (pretty sure it's meant to be private that's why u need getter and setter methods)
     // set as boolean cause either logged in or logged out
         // true = logged in
         // false = logged out
     private static Boolean CurrentUser;
 
-    // constructor
-    public SessionManager(Boolean CurrentUser){
-        this.CurrentUser = CurrentUser;
-    }
+    // no constructor needed static variables belong to classes not objects with constructors initialising objects
+
     public Boolean GetCurrentUser() {
         return GetCurrentUser();
     }
     public void SetCurrentUser_In(Boolean CurrentUser){
         CurrentUser = true;
-        this.CurrentUser = CurrentUser;
     }
     public void SetCurrentUser_Out(Boolean CurrentUser){
         CurrentUser = false;
-        this.CurrentUser = CurrentUser;
     }
 }

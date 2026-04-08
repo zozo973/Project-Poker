80c36e90d23cacb90ffbb2541cf9c96f6c044064
Basilis Karalouka <basi.loukas1@gmail.com>
2026-04-08 22:21:02 +1000

    adding constructor to user class

--- src/main/java/com/example/projectpoker/model/User.java
+++ src/main/java/com/example/projectpoker/model/User.java
@@ -3,9 +3,23 @@ package com.example.projectpoker.model;
 import java.time.LocalDateTime;
 
 public class User {
+    // fields that go into the User Database table
     private int UserID;
     private String userName;
     private String userPassword;
     private String email;
     private LocalDateTime createdAt;
+
+    // add constructor to set initial values for object fields
+    // method takes parameters which will be set by user
+    private User(String userName, String userPassword, String email) {
+        this.userName = userName;
+        this.userPassword = userPassword;
+        this.email = email;
+    }
+
 }
+
+
+
+

114050472a0acd062d817554e57c196a209af9dc
Hugo Powe <11580178@qut.edu.au>
2026-04-08 22:26:17 +1000

    merged propertyChangeSupport feature and am Commiting master to push to remote rempo

--- src/main/java/com/example/projectpoker/model/oberserver/Observer.java
+++
@@ -1,5 +0,0 @@
-package com.example.projectpoker.model.oberserver;
-
-public interface Observer {
-    void update();
-}
--- src/main/java/com/example/projectpoker/model/oberserver/Subject.java
+++
@@ -1,7 +0,0 @@
-package com.example.projectpoker.model.oberserver;
-
-public interface Subject {
-    void addObserver(Observer observer);
-    void removeObserver(Observer observer);
-    void notifyObservers();
-}

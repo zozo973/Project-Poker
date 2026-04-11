c57b144c361caf7385f3bfdf5cde83e69f927916
Basilis Karalouka <basi.loukas1@gmail.com>
2026-04-11 21:03:46 +1000

    added functionality to handle register method

--- src/main/java/com/example/projectpoker/controller/RegisterController.java
+++ src/main/java/com/example/projectpoker/controller/RegisterController.java
@@ -10,24 +10,31 @@ import javafx.scene.control.TextField;
 
 public class RegisterController {
 
-    @FXML
-    private TextField usernameField;
-    @FXML
-    private TextField emailField;
-    @FXML
-    private PasswordField passwordField;
-    @FXML
-    private PasswordField confirmPasswordField;
-    @FXML
-    private Label messageLabel;
+    @FXML private TextField usernameField;
+    @FXML private TextField emailField;
+    @FXML private PasswordField passwordField;
+    @FXML private PasswordField confirmPasswordField;
+    @FXML private Label messageLabel;
 
     private void handleRegister() {
+        String username = usernameField.getText();
+        String email =  emailField.getText();
+        String password = passwordField.getText();
+
+        boolean checkedUsername = username.isBlank();
+        boolean checkedEmail = email.isBlank();
+        boolean checkedPassword = password.isBlank();
+
+        if (checkedUsername || checkedEmail || checkedPassword) {
+            // display something like please invalid entry please fill all fields
+            // make sure nothing changes or username, email and password fields are reset so it's empty for the user to retype them out
+        }
 
 
 
     }
 
     private void goToLogin() {
-
+        
     }
 }

0e0eab7ba352994567e4919528649d92f30afb68
Basilis Karalouka <basi.loukas1@gmail.com>
2026-04-11 20:28:00 +1000

    added fxml register code and started on RegisterController.java

--- src/main/java/com/example/projectpoker/controller/RegisterController.java
+++ src/main/java/com/example/projectpoker/controller/RegisterController.java
@@ -1,4 +1,33 @@
 package com.example.projectpoker.controller;
 
+import com.example.projectpoker.database.UserDAO;
+import com.example.projectpoker.model.User;
+import com.example.projectpoker.service.PasswordHasher;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 public class RegisterController {
+
+    @FXML
+    private TextField usernameField;
+    @FXML
+    private TextField emailField;
+    @FXML
+    private PasswordField passwordField;
+    @FXML
+    private PasswordField confirmPasswordField;
+    @FXML
+    private Label messageLabel;
+
+    private void handleRegister() {
+
+
+
+    }
+
+    private void goToLogin() {
+
+    }
 }

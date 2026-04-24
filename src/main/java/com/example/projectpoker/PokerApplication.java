c093d0a9b99b14840455ae5595e761b9091b4736
Basilis Karalouka <basi.loukas1@gmail.com>
2026-04-24 13:16:35 +1000

    fixing some hardcoded implementation in PokerApplication.java

--- src/main/java/com/example/projectpoker/PokerApplication.java
+++ src/main/java/com/example/projectpoker/PokerApplication.java
@@ -59,11 +59,13 @@ public class PokerApplication extends Application {
 
         User loggedInUser = SessionManager.getCurrentUser();
         Player user = new Player(loggedInUser.getUsername(), loggedInUser.getCurrentBalance());
-//        int userBuyIn = 1000;
+        // int userBuyIn = 1000;
         int userBuyIn = loggedInUser.getCurrentBalance();
 
+        // calculate blind
         int blind = safeRoundToInt((userBuyIn*0.03));
         int numPlayer = 4;
+        // create game
         Game game = new Game(user,userBuyIn,4,blind,10,40, Difficulty.GAMBLINGADDICT);
 
         RoundController controller = (RoundController) loadPokerGameView(game);

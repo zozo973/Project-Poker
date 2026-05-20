1c65bbf97b1fa5636d4232959c967f7bc2d87edf
Gattfo <gattfo@hotmail.com>
2026-05-20 15:16:17 +1000

    Changed TablePosition nameplate coordinate so long plates cant render over cards.

--- src/main/java/com/example/projectpoker/model/game/Round.java
+++ src/main/java/com/example/projectpoker/model/game/Round.java
@@ -568,9 +568,6 @@ public class Round {
 
         waitForPlayerDecision(activePlayer);
         activePlayer.play(this.pots);
-        if (activePlayer.getAction() == Action.UNDECIDED) {
-            autoResolvePlayerDecision(activePlayer, false);
-        }
     }
 
     private AIActions.AiPlayerResponse getAiDecision(AiPlayer aiPlayer) {
